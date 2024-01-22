53e9f7fe58fabe356d60211c771aa1f3705ba20d
Ahmedradwancs <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-01-22 17:10:16 +0100

    Add project files.

---
+++ Pet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Pet
+    {
+        private String name;
+        private int age;
+        private bool isFemale;
+
+        // Read input from the user
+        public void ReadAndSavePetData()
+        {
+            Console.WriteLine("Enter the name of your pet: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the age of your pet: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Is your pet a female (y/n) ? ");
+            if (Console.ReadLine().ToLower() == "y")
+            {
+                isFemale = true;
+            }
+            else
+            {
+                isFemale = false;
+            }
+        }
+
+        // Print the data to the console
+        public void DisplayPetInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("+++++++++++++++++++++++++");
+            Console.WriteLine($"Name: {this.name} Age: {this.age}");
+            if (this.isFemale == true)
+            {
+                Console.WriteLine("She's such a wonderful pup!");
+            }
+            else
+            {
+                Console.WriteLine("He's such a wonderful pup!");
+            }
+            Console.WriteLine("+++++++++++++++++++++++++");
+
+        }
+
+        // Create the start method
+        public void Start()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Greetings from the pet CLass!");
+            Console.WriteLine();
+
+            ReadAndSavePetData();
+            DisplayPetInfo();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupConsoleWindow();
+            Console.Title = "My Favourite Pet";
+
+            // Create a Pet object
+            Pet pet = new Pet();
+            
+            // Call the Start method
+            pet.Start();
+
+            Console.WriteLine("Press Enter to start next part!");
+            Console.ReadLine();
+        }
+
+        static void SetupConsoleWindow()
+        {
+            // Arranging the console window            
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "My Console Classes";
+        }
+    }
+}
